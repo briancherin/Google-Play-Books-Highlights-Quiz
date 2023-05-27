4d4abad25e093e219fe9e5667c410e91deac0dc6
Brian Cherin <brian.cherin@gmail.com>
2023-05-27 14:49:49 -0400

    #5 Change linear progress bar to circular progress bar for import

--- src/Components/ProgressCard.js
+++ src/Components/ProgressCard.js
@@ -5,10 +5,10 @@ const ProgressCard = (props) => {
     return(
         <Card style={{flexGrow: 1, backgroundColor:"white", padding:30}}>
             <Typography variant="h5" style={{textAlign: "center", paddingBottom:10}}>Loading highlights</Typography>
-            <LinearProgress variant="determinate" value={props.progress}/>
-            {/* <div style={{flexGrow: 1, display:"flex", alignItems: "center"}}> */}
-                {/* <CircularProgress /> */}
-            {/* </div> */}
+            {/*<LinearProgress variant="determinate" value={props.progress}/>*/}
+             <div style={{flexGrow: 1, display:"flex", justifyContent: "center", alignItems: "center"}}>
+                 <CircularProgress />
+             </div>
         </Card>
     );
 }
